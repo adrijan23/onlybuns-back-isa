3048977509648135050d6b656ba624e168906f0f
Adrijan Radjevic <adrijan.radjevic@gmail.com>
2024-10-25 13:54:45 +0200

    Initial commit

---
+++ onlybuns/src/main/java/team5/onlybuns/OnlybunsApplication.java
@@ -0,0 +1,13 @@
+package team5.onlybuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlybunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlybunsApplication.class, args);
+	}
+
+}
