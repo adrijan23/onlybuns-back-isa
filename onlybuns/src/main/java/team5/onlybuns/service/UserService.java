9facb774aae2c365f70ccb1633675a8411c5a072
Zeka5 <zekovicognjen753@gmail.com>
2024-11-16 17:16:19 +0100

    feat: users pagination

--- onlybuns/src/main/java/team5/onlybuns/service/UserService.java
+++ onlybuns/src/main/java/team5/onlybuns/service/UserService.java
@@ -3,6 +3,7 @@ package team5.onlybuns.service;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.data.domain.Page;
 import team5.onlybuns.dto.UserRequest;
 import team5.onlybuns.model.User;
 
@@ -12,6 +13,7 @@ public interface UserService {
     List<User> findAll ();
 	User save(UserRequest userRequest);
     void update(User user);
+    Page<User> getPaginated (int page, int size);
 //    Set<User> getFollowers(Long id);
 //    Set<User> getFollowing(Long id);
 //    void follow(Long userId, Long targetId);
