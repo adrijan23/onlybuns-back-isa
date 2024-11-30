0738fe546c3b3acf9e85d95f2c5522d70443385b
Adrijan Radjevic <adrijan.radjevic@gmail.com>
2024-11-30 11:23:47 +0100

    Implemented rate limiter with EhCache

--- onlybuns/src/main/java/team5/onlybuns/OnlybunsApplication.java
+++ onlybuns/src/main/java/team5/onlybuns/OnlybunsApplication.java
@@ -2,11 +2,13 @@ package team5.onlybuns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @EnableTransactionManagement
 public class OnlybunsApplication {
 
